a4596874795d454331d1ff4f988995947fcdbacb
Reggie Nillo <30332785+RJMNillo@users.noreply.github.com>
2021-02-28 20:42:00 -1000

    Created a working and passing orders test file and began making website script (should have been done in hw3)

---
+++ hw3/public/javascripts/WebsiteScript.js
@@ -0,0 +1,1 @@
+//Reggie Jan Marc Nillo
---
+++ hw3/tests/orders.test.js
@@ -0,0 +1,9 @@
+const orders = require('../routes/orders.js');
+//Create a test object like one in orders.js
+var TestOrder = {Topping: 'Test', Quantity: 0};
+var TestJSON = JSON.stringify(TestOrder);
+//Check if an order for one of the toppings have a string and number
+test('Class type checking', () => 
+    {
+        expect(TestJSON).toEqual(expect.stringContaining('Topping'));
+    });
