0de18a51723ab058c28375d365b393ddc2e4dbff
Reggie Nillo <30332785+RJMNillo@users.noreply.github.com>
2021-03-01 16:25:28 -1000

    index changed and orders.js edited

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var PlainOrders = {Topping: 'Plain',Quantity: 6};
-var ChocOrders = {Topping: 'Chocolate',Quantity: 6};
-var StrawOrders = {Topping: 'Strawberry',Quantity: 6};
+var PlainOrders = JSON.stringify({Topping: 'Plain',Quantity: 6});
+var ChocOrders = JSON.stringify({Topping: 'Chocolate',Quantity: 6});
+var StrawOrders = JSON.stringify({Topping: 'Strawberry',Quantity: 6});
 var JanOrders = [PlainOrders, ChocOrders, StrawOrders];
 
 /* GET users listing. */
